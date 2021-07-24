92e9b3670f35e94603c8f18439ff210ed2417f9a
timointhebush <junzzang97@naver.com>
2021-07-25 00:40:26 +0900

    2020카카오인턴 보석쇼핑 성공

--- PROGRAMMERS/2020카카오인턴_보석쇼핑.py
+++ PROGRAMMERS/2020카카오인턴_보석쇼핑.py
@@ -1,27 +1,30 @@
 def solution(gems):
     ans = []
-    n = len(gems)
-    numOfTypes = len(set(gems))
     l, r = 0, 0
-    numOfGems = {gems[l] : 1}
-    while l < n and r < n:
-        if len(numOfGems) < numOfTypes:
-            if r+1 <n:
-                r += 1
-                gem  = gems[r]
-                if gem in numOfGems:
-                    numOfGems[gem] += 1
-                else:
-                    numOfGems[gem] = 1
-        else: # len(numOfGems) = numOfTypes
-            ans.append((l, r))
+    numOfKind, numOfGem = len(set(gems)), {gems[r] : 1}
+    lenOfDis = len(gems)
+    while True:
+        if len(numOfGem) < numOfKind:
+            r += 1
+            if r >= lenOfDis:
+                break
+            gem = gems[r]
+            if gem not in numOfGem:
+                numOfGem[gem] = 1
+            else:
+                numOfGem[gem] += 1
+        else: # len(numOfGem) = numOfKind
+            if l >= lenOfDis:
+                break
+            ans.append( (l, r) )
             gem = gems[l]
-            numOfGems[gem] -= 1
-            if numOfGems[gem] == 0:
-                del numOfGems[gem]
+            numOfGem[gem] -= 1
+            if numOfGem[gem] == 0:
+                del numOfGem[gem]
             l += 1
-            
-    return(ans)
+    ans = sorted(ans, key=lambda x : (x[1]-x[0], x[0]))
+    a, b = ans[0]
+    return( [a+1, b+1] )
 
 print(solution(["DIA", "RUBY", "RUBY", "DIA", "DIA", "EMERALD", "SAPPHIRE", "DIA"]	))
 print(solution(["AA", "AB", "AC", "AA", "AC"]))
