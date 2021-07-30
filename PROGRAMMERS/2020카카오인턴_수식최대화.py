86d44b0173f66a90a7d7d77d851ddbfc4c4df154
timointhebush <junzzang97@naver.com>
2021-07-31 00:50:27 +0900

    2020카카오인턴_수식최대화 성공

--- PROGRAMMERS/2020카카오인턴_수식최대화.py
+++ PROGRAMMERS/2020카카오인턴_수식최대화.py
@@ -1,19 +1,32 @@
 def solution(expression):
     answer = 0
-    numList, oprtList = splitExp(expression)
-
-
+    numList, oprtList = splitExp(expression, ["*", "+", "-"])
+    cases, operator = createOprtCases(list(set(oprtList)))
+    for case in cases:
+        numListCopy, oprtListCopy = numList.copy(), oprtList.copy()
+        for idx in case:
+            opt = operator[idx]
+            while True:
+                if oprtListCopy.count(opt) == 0:
+                    break
+                opt_idx = oprtListCopy.index(opt)
+                result = cal( numListCopy[opt_idx], numListCopy[opt_idx+1], opt )
+                del numListCopy[opt_idx+1]
+                del numListCopy[opt_idx]
+                del oprtListCopy[opt_idx]
+                numListCopy.insert(opt_idx, result)
+        if abs(numListCopy[0]) > answer:
+            answer = abs(numListCopy[0])
     return answer
 
 def createOprtCases(oprt):
     n = len(oprt)
     if n == 3:
-        n = 6
+        return [ (0, 1, 2), (0, 2, 1), (1, 0, 2), (1, 2, 0), (2, 1, 0), (2, 0, 1) ], oprt
     elif n == 2:
-        n = 4
+        return [ (0, 1), (1, 0) ], oprt
     else:
-        n = 1
-    cases = [ {} for i in range(oprt) ]
+        return [ (0, ) ], oprt
 
 def cal(a, b, oprt):
     if oprt == "+":
@@ -23,9 +36,8 @@ def cal(a, b, oprt):
     else:
         return a - b         
 
-def splitExp(expression):
+def splitExp(expression, operator):
     start = 0
-    operator = ['-', '*', '+']
     numList = []
     oprtList = []
     for end in range(len(expression)):
@@ -38,3 +50,6 @@ def splitExp(expression):
 
 print(solution("100-200*300-500+20"))
 print(solution("50*6-3*2"))
+print(solution("200-300-500-600*40+500+500"))
+print(solution("2-990-5+2"))
+print(solution("2*2"))
