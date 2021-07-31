f4c8e1c2990941589e28a84869338998c99a496d
timointhebush <junzzang97@naver.com>
2021-07-31 23:10:02 +0900

    2020 카카오인턴 경주로 건설

--- PROGRAMMERS/2020카카오인턴_경주로건설.py
+++ PROGRAMMERS/2020카카오인턴_경주로건설.py
@@ -1,17 +1,23 @@
+from collections import deque
+
 def solution(board):
-    answer = 0
     n = len(board)
-    for row in range(n):
-        for col in range(n):
-            if col == 0 and row == 0:
-                pass
-            candid1 = 
+    queue = deque()
+    queue.append((0, 0, 0, 2))
+    queue.append((0, 0, 0, 3))
+    visited = {(0,0,2):True, (0,0,3):True}
+    moves = [ (-1, 0), (1, 0), (0, -1), (0, 1) ]
+    while len(queue) != 0:
+        row, col, cost ,dirc = queue.popleft()
+        for move in moves:
+            m_row, m_col = move
+            n_row, n_col = row + m_row, col + m_col
+            if n_row < 0 or n_col < 0 or n_row >= n or n_col >= n or (n_row, n_col, dirc) in visited:
+                break
+            visited[ (n_row, n_col, dirc) ] = True
+            
 
-
-
+    visited = {}
+    ans_board = [ [  ]   ]
+    answer = 0
     return answer
-    
-def setCnadid(row, col, board):
-    
-
-def decoder():
