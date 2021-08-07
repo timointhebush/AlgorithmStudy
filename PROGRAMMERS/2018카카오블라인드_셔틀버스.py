928ff77359c8362dea0e7799c8a0562e6a18190a
timointhebush <junzzang97@naver.com>
2021-08-07 17:37:40 +0900

    2018카카오블라인드 셔틀버스 성공

--- PROGRAMMERS/2018카카오블라인드_셔틀버스.py
+++ PROGRAMMERS/2018카카오블라인드_셔틀버스.py
@@ -1,36 +1,27 @@
-def solution(n, t, m, timetable):
-    answer = ''
-    timetableM = []
-    for time in timetable:
-        if time != "23:59":
-            tmp = time.split(":")
-            timetableM.append(int(tmp[0])*60 + int(tmp[1]))
-    timetableM = sorted(timetableM)
-    if len(timetableM) == 0:
-        return toTime(9*60 + (n-1)*t)
-    
-    busTable = []
-    for num in range(n):
-        busTable.append(9*60 + (num-1)*t)
-    
-    boarded = []
-    for busT in busTable:
-        for _ in range(m):
-            if not timetableM:
-                return toTime(9*60 + (n-1)*t)
-            if timetableM[0] > busT:
-                break
-            boarded.append(timetableM[0])
-            timetableM.pop(0)
+from collections import deque
 
-    if timetableM:
-        return toTime(timetableM[-1]-1)
-    else:
-        return toTime(brink - 1) 
+def solution(n, t, m, timetable):
+    timetable = deque(sorted(map(toMinute, timetable)))
+    for N in range(n):
+        bus = []
+        M = 0
+        currentBus = 540 + t*N
+        while timetable and M < m and timetable[0] <= currentBus:
+            bus.append(timetable.popleft()) 
+            M += 1
+        if N == n-1: # 마지막
+            if len(bus) < m:
+                return(toTime(currentBus))
+            else:
+                return(toTime(bus[-1] - 1))
 
 def toTime(minute):
     hour = minute // 60
     return f"{hour:02d}:{minute%60:02d}"
+
+def toMinute(time):
+    tmp = time.split(":")
+    return int(tmp[0])*60 + int(tmp[1])
 
 print(solution(1, 1, 5, ["08:00", "08:01", "08:02", "08:03"]))
 print(solution(2, 1, 2, ["09:00", "09:00", "09:00", "09:00"]))
