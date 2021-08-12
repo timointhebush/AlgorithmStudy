646ee04a02f5608dad080337c1713932afec817b
timointhebush <junzzang97@naver.com>
2021-08-13 00:33:25 +0900

    2020카카오블라인드 가사검색 실패

--- PROGRAMMERS/2020카카오블라인드_가사검색.py
+++ PROGRAMMERS/2020카카오블라인드_가사검색.py
@@ -1,65 +1,97 @@
+class Node:
+    def __init__(self, value, depth):
+        self.value = value
+        self.child = None
+        self.depth = depth
+    
+    def findValue(self, value):
+        if self.child == None:
+            return None
+        else:
+            for node in self.child:
+                if node.value == value:
+                    return node
+            return False
+    
+    def getLeafNum(self, depth):
+        if self.child == None:
+            return 0
+        stack = [self]
+        targetDepth = self.depth + depth
+        num = 0
+        while len(stack) != 0:
+            node = stack.pop()
+            if node.child == None:
+                if node.depth == targetDepth:
+                    num += 1
+            else:
+                for childNode in node.child:
+                    stack.append(childNode)
+                    if childNode.child == None and childNode.depth == targetDepth:
+                        num += 1
+        return num
+            
+class Tree:
+    def __init__(self):
+        self.root = Node(None, 0)
+
+
 def solution(words, queries):
     answer = []
-    hist = {}
+    tree = createTree(Tree(), words)
+    reverseTree = createReverseTree(Tree(), words)
     for query in queries:
-        num = 0
-        tmp = []
-        hist_queries = createHistQueries(query)
-        for hist_query in hist_queries:
-            if hist_query in hist:
-                search_list = hist[hist_query]
-                break
-            else:
-                search_list = words
-            
-        for word in search_list:
-            if check(query, word):
-                num += 1
-                tmp.append(word)
-        answer.append(num)
-        hist[query] = tmp
+        if query[0] == "?":
+            answer.append(checkTree(reverseTree, query[::-1]))
+        else:
+            answer.append(checkTree(tree, query))
     return answer
 
-def check(query, word):
-    q, w = len(query), len(word)
-    if q != w :
-        return False
-    idx = 0
-    while idx <= q-1:
-        if query[idx] != word[idx] and query[idx] != "?":
-            return False
-        idx += 1
-    return True
+def createTree(tree, words):
+    for word in words:
+        n = len(word)
+        node = tree.root
+        for i in range(n):
+            childNode = node.findValue(word[i])
+            if childNode == None:
+                node.child = [ Node(word[i], i+1) ]
+            elif childNode == False:
+                node.child.append(Node(word[i], i+1))
+            else:
+                node = childNode
+    return tree
 
-def createHistQueries(query):
-    hist_queries = []
+def createReverseTree(tree, words):
+    for word in words:
+        n = len(word)
+        node = tree.root
+        depth = 1
+        for i in range(n-1, -1, -1):
+            childNode = node.findValue(word[i])
+            if childNode == None:
+                node.child = [ Node(word[i], depth) ]
+            elif childNode == False:
+                node.child.append(Node(word[i], depth))
+            else:
+                node = childNode
+            depth += 1
+    return tree
+
+def checkTree(tree, query):
     n = len(query)
-    q_find_idx = query.index("?")
-    idx = 0
-    if query[0] != "?": # 앞쪽이 알파벳으로 시작
-        for find_idx in range(q_find_idx, 0, -1):
-            hist_query = ""
-            for i, v in enumerate(query):
-                if i == find_idx-1:
-                    hist_query += "?"
-                else:
-                    hist_query += v
-            hist_queries.append(hist_query)
-    else: # 뒷쪾이 알파벳으로 시작
-        while True:
-            hist_query = ""
-            met = False
-            for i, v in enumerate(query):
-                if v != "?" and met == False:
-                    hist_query += "?"
-                    met = True
-                else:
-                    hist_query += v
-            hist_queries.append(hist_query)
-            if hist_query[-1] == "?":
-                break
-            query = hist_query
-    return hist_queries
-                
+    node = tree.root
+    for i in range(n):
+        if query[i] == "?":
+            depthLeft = n - i
+            return node.getLeafNum(depthLeft)
+        else:
+            childNode = node.findValue(query[i])
+            if childNode == None or childNode == False:
+                return 0
+            else:
+                node = childNode
+
+
+
 print(solution(["frodo", "front", "frost", "frozen", "frame", "kakao"], ["fro??", "????o", "fr???", "fro???", "pro?"]))
     
