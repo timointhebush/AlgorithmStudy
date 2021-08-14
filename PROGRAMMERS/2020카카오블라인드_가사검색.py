98d306fced20e28211df65a3787dafcc70e06c61
timointhebush <junzzang97@naver.com>
2021-08-14 13:57:41 +0900

    2020카카오블라인드 가사검색 효율성3번실패

--- PROGRAMMERS/2020카카오블라인드_가사검색.py
+++ PROGRAMMERS/2020카카오블라인드_가사검색.py
@@ -1,109 +1,60 @@
 class Node:
-    def __init__(self, value, depth):
-        self.value = value
-        self.child = None
-        self.depth = depth
-    
-    def findValue(self, value):
-        if self.child == None:
-            return None
-        else:
-            for node in self.child:
-                if node.value == value:
-                    return node
-            return False
-    
-    def getLeafNum(self, depth):
-        if self.child == None:
-            return 0
-        stack = [self]
-        targetDepth = self.depth + depth
-        num = 0
-        while len(stack) != 0:
-            node = stack.pop()
-            if node.child != None:
-                for childNode in node.child:
-                    stack.append(childNode)
-                    if childNode.child == None and childNode.depth == targetDepth:
-                        num += 1
-        return num
-            
+    def __init__(self):
+        self.count = 1
+        self.child = {}
+
 class Tree:
     def __init__(self):
-        self.root = Node(None, 0)
+        self.root = Node()
+
+    def addWord(self, word):
+        node = self.root
+        for w in word:
+            if w in node.child:
+                node = node.child[w]
+                node.count += 1
+            else:
+                node.child[w] = Node()
+                node = node.child[w]
+
+    def getCount(self, partOfQuery):
+        node = self.root
+        for q in partOfQuery:
+            if q in node.child:
+                node = node.child[q]
+            else:
+                return 0
+        return node.count
 
 def solution(words, queries):
     answer = []
-    tree = createTree(Tree(), words)
-    reverseTree = createReverseTree(Tree(), words)
+    forwardTrees, reverseTrees = {}, {}
+    for word in words:
+        n = len(word)
+        if n in forwardTrees:
+            forwardTree, reverseTree = forwardTrees[n], reverseTrees[n]
+            forwardTree.addWord(word)
+            reverseTree.addWord(word[::-1])
+        else:
+            forwardTree, reverseTree = Tree(), Tree()
+            forwardTree.addWord(word)
+            reverseTree.addWord(word[::-1])
+            forwardTrees[n], reverseTrees[n] = forwardTree, reverseTree
     for query in queries:
-        if query[0] == "?":
-            answer.append(checkTree(reverseTree, query[::-1]))
+        n = len(query)
+        if n in forwardTrees:
+            if query[0] != "?": # 정방향
+                splitedQuery = query.split("?")
+                answer.append(forwardTrees[n].getCount(splitedQuery[0]))
+            else:
+                if query[n-1] == "?":
+                    answer.append(reverseTrees[n].root.count)
+                else:
+                    splitedQuery = query[::-1].split("?")
+                    answer.append(reverseTrees[n].getCount(splitedQuery[0]))
         else:
-            answer.append(checkTree(tree, query))
+            answer.append(0)
     return answer
-
-def createTree(tree, words):
-    for word in words:
-        n = len(word)
-        node = tree.root
-        for i in range(n):
-            childNode = node.findValue(word[i])
-            if childNode == None:
-                node.child = [ Node(word[i], i+1) ]
-                node = node.child[0]
-            elif childNode == False:
-                newNode = Node(word[i], i+1)
-                node.child.append(newNode)
-                node = newNode
-            else:
-                node = childNode
-    return tree
-
-def createReverseTree(tree, words):
-    for word in words:
-        n = len(word)
-        node = tree.root
-        depth = 1
-        for i in range(n-1, -1, -1):
-            childNode = node.findValue(word[i])
-            if childNode == None:
-                node.child = [ Node(word[i], depth) ]
-                node = node.child[0]
-            elif childNode == False:
-                newNode = Node(word[i], depth)
-                node.child.append(newNode)
-                node = newNode
-            else:
-                node = childNode
-            depth += 1
-    return tree
-
-def checkTree(tree, query):
-    n = len(query)
-    node = tree.root
-    for i in range(n):
-        if query[i] == "?":
-            depthLeft = n - i
-            return node.getLeafNum(depthLeft)
-        else:
-            childNode = node.findValue(query[i])
-            if childNode == None or childNode == False:
-                return 0
-            else:
-                node = childNode
-
-def BFS(tree):
-    from collections import deque
-    node = tree.root
-    queue = deque([node])
-    while len(queue) != 0:
-        node = queue.popleft()
-        if node.child != None:
-            print(node.value, "의 자식들")
-            for childNode in node.child:
-                print("value: ", childNode.value, "--depth: ", childNode.depth)
-                queue.append(childNode)
 
 print(solution(["frodo", "front", "frost", "frozen", "frame", "kakao"], ["fro??", "????o", "fr???", "fro???", "pro?"]))
     
