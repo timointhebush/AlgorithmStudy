b02fac4c0dc42b1c7b0e7507d5236298252095be
timointhebush <junzzang97@naver.com>
2021-08-21 19:09:24 +0900

    2020카카오 블라인드 블록이동하기 실패

--- PROGRAMMERS/2020카카오블라인드_블록이동하기.py
+++ PROGRAMMERS/2020카카오블라인드_블록이동하기.py
@@ -1,13 +1,107 @@
 def solution(board):
     from collections import deque
     answer = 0
+    n = len(board)
     # tuple의 0번째를 tail, 1번째를 head
-    robot = ( (0,0), (0,1) )
-    queue = deque( getNextMoves() )
+    timeBoard = [ [float('inf') for _ in range(n)] for _ in range(n) ]
+    queue = deque( [ ( (0,0), (0,1), 0 ) ] )
     while len(queue) != 0:
-        
+        start = queue.popleft()
+        nextMoves = getNextMoves(start, board)
+        if nextMoves:
+            for connected in nextMoves:
+                if isLessThan(timeBoard, connected):
+                    updateTimeBoard(timeBoard, connected)
+                    queue.append(connected)
+        # for b in timeBoard:
+        #     print(b)
+        # print("  ")
+    return timeBoard[-1][-1]
 
+def getNextMoves(robot, board):
+    nextMoves = getParallerMoves(robot, board) + getRotateMoves(robot, board)
+    nextMoves = filterMoves(nextMoves, board)
+    return nextMoves
 
-    return answer
+def getParallerMoves(robot, board):
+    partA, partB, time = robot
+    moves = []
+    #up
+    moves.append( ( (partA[0]-1, partA[1]), (partB[0]-1, partB[1]), time+1 ) )
+    #down
+    moves.append( ( (partA[0]+1, partA[1]), (partB[0]+1, partB[1]), time+1 ) )
+    #left
+    moves.append( ( (partA[0], partA[1]-1), (partB[0], partB[1]-1), time+1 ) )
+    #right
+    moves.append( ( (partA[0], partA[1]+1), (partB[0], partB[1]+1), time+1 ) )
+    return moves
+
+def getRotateMoves(robot, board):
+    partA, partB, time = robot
+    if partA[0] == partB[0]: # 가로 방향
+        moves = getHorizonRotate(robot, board)
+    else:
+        moves = getVerticalRoatae(robot, board)
+    return moves
+
+def getHorizonRotate(robot, board):
+    partA, partB, time = robot
+    moves = []
+    if isCoordOut(partA[0]-1, partA[1]+1, board) == False and board[partA[0]-1][partA[1]+1] != 1:
+        moves.append( ( (partA[0], partA[1]), (partB[0]-1, partB[1]-1), time+1 ) )
+    if isCoordOut(partA[0]+1, partA[1]+1, board) == False and board[ partA[0]+1 ][ partA[1]+1 ] != 1:
+        moves.append( ( (partA[0], partA[1]), (partB[0]+1, partB[1]-1), time+1 ) )
+    if isCoordOut( partB[0]-1 , partB[1]-1, board) == False and board[ partB[0]-1 ][ partB[1]-1 ] != 1:
+        moves.append( ( (partA[0]-1, partA[1]+1), (partB[0], partB[1]), time+1 ) )
+    if isCoordOut( partB[0]+1 , partB[1]-1, board ) == False and board[ partB[0]+1 ][ partB[1]-1 ] != 1:
+        moves.append( ( (partA[0]+1, partA[1]+1), (partB[0], partB[1]), time+1 ) )
+    return moves
+
+def getVerticalRoatae(robot, board):
+    partA, partB, time = robot
+    moves = []
+    if isCoordOut(partA[0]+1, partA[1]+1, board) == False and board[partA[0]+1][partA[1]+1] != 1:
+        moves.append( ( (partA[0], partA[1]), (partB[0]-1, partB[1]+1), time+1 ) )
+    if isCoordOut(partA[0]+1, partA[1]-1, board) == False and board[ partA[0]+1 ][ partA[1]-1 ] != 1:
+        moves.append( ( (partA[0], partA[1]), (partB[0]-1, partB[1]-1), time+1 ) )
+    if isCoordOut(partB[0]-1 , partB[1]+1, board) == False and board[partB[0]-1 ][ partB[1]+1] != 1:
+        moves.append( ( (partA[0]+1, partA[1]+1), (partB[0], partB[1]), time+1 ) )
+    if isCoordOut(partB[0]-1 , partB[1]-1, board) == False and board[partB[0]-1 ][ partB[1]-1] != 1:
+        moves.append( ( (partA[0]+1, partA[1]-1), (partB[0], partB[1]), time+1 ) )
+    return moves
+
+def isCoordOut(row, col, board):
+    n = len(board)
+    if row < 0 or col < 0 or row >= n or col >= n:
+        return True
+    return False
+
+def isCoordWall(row, col, board):
+    if isCoordOut(row, col, board):
+        return True
+    if board[row][col] == 1:
+        return True
+    return False
+
+def filterMoves(nextMoves, board):
+    moves = []
+    for move in nextMoves:
+        partA, partB, time = move
+        if isCoordOut(partA[0], partA[1], board) or isCoordOut(partB[0], partB[1], board) or isCoordWall(partA[0], partA[1], board) or isCoordWall(partB[0], partB[1], board):
+            pass
+        else:
+            moves.append(move)
+    return moves
+
+def isLessThan(timeBoard, connected):
+    partA, partB, time = connected
+    if timeBoard[partA[0]][partA[1]] >= time or timeBoard[partB[0]][partB[1]] >= time:
+        return True
+    return False
+
+def updateTimeBoard(timeBoard, connected):
+    partA, partB, time = connected
+    timeBoard[partA[0]][partA[1]] = time
+    timeBoard[partB[0]][partB[1]] = time
 
 print(solution([[0, 0, 0, 1, 1],[0, 0, 0, 1, 0],[0, 1, 0, 1, 1],[1, 1, 0, 0, 1],[0, 0, 0, 0, 0]]))
