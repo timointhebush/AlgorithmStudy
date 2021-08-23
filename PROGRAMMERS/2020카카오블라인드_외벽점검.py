7745d05a2b55a2612d6774e0cd203ffea1e25d62
timointhebush <junzzang97@naver.com>
2021-08-23 13:38:42 +0900

    2020 카카오 블라인드 외벽점검 성공

--- PROGRAMMERS/2020카카오블라인드_외벽점검.py
+++ PROGRAMMERS/2020카카오블라인드_외벽점검.py
@@ -1,20 +1,31 @@
+from itertools import permutations
+
 def solution(n, weak, dist):
-    answer = 0
-    d = len(dist)
-    for i in range(-1, -d-1, -1):
-        if tryCheck(n, weak, dist[i:]) == True:
-            return -1 * i
-    return -1
+    w_num = len(weak)
+    cand = []
+    # weak을 선형 구조로 생성
+    new_weak = [ n+w for w in weak ]
+    weak += new_weak
+    friends_list = permutations(dist)
+    # friends_list = [ (40,30,10,5,1) ]
+    for friends in friends_list:
+        for start_idx in range(w_num):
+            pos = weak[start_idx]
+            for f_num, friend in enumerate(friends):
+                pos += friend
+                if pos >= weak[ start_idx + w_num - 1 ]:
+                    cand.append(f_num+1)
+                    break
+                else:
+                    for w in weak:
+                        if w > pos:
+                            pos = w
+                            break
+    # print(cand)
+    return -1 if len(cand) == 0 else min(cand)
 
-def tryCheck(n, weak, dsitPart):
-    for w in weak:
-        checked = {}
-        for d in dsitPart[::-1]:
-            clockP = w + d
-            checkBetween(n, weak, start, clockP)
-            counterClockP = w - d
 
-def checkBetween(n, weak, start, point):
-    if point 
 
-print(solution(12, [1, 5, 6, 10], [1, 2, 3, 4]))
+# print(solution(12, [1, 5, 6, 10], [1, 2, 3, 4]))
+# print(solution(30, [0, 3, 11, 21], [10, 4]))
+print(solution(200, [0, 10, 50, 80, 120, 160], [1, 5, 10, 30, 40]))
