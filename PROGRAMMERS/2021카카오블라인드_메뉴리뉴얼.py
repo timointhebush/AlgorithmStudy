6d2d692e53d923ebde6306097152d33b15046a39
timointhebush <junzzang97@naver.com>
2021-08-27 01:23:43 +0900

    2021카카오블라인드 메뉴리뉴얼 실패2

--- PROGRAMMERS/2021카카오블라인드_메뉴리뉴얼.py
+++ PROGRAMMERS/2021카카오블라인드_메뉴리뉴얼.py
@@ -1,8 +1,37 @@
 def solution(orders, course):
     answer = []
     n = len(orders)
+    intersections_cand = []
+    for i in range(n):
+        for j in range(i+1, n):
+            intersection = set(orders[i])&set(orders[j])
+            if intersection not in intersections_cand and len(intersection) >= 2:
+                intersections_cand.append(intersection)
+    intersections_cand = sorted(intersections_cand, key=lambda x : len(x))
+    # print(intersections_cand)
+
+    len_cand = {}
+    for i in range(course[0], course[-1]+1):
+        len_cand[i] = []
+
+    for intersec in intersections_cand:
+        num = 0
+        for order in orders:
+            if intersec & set(order) == intersec:
+                num += 1
+        len_cand[ len(intersec) ].append( (intersec, num) )
+    # print(len_cand)
     
+    for c in course:
+        cand = len_cand[c]
+        if len(cand) != 0:
+            max_num = max(cand, key=lambda x : x[1])[1]
+            for intersec, num in cand:
+                if num == max_num:
+                    answer.append( "".join(sorted(list(intersec))) )
+    answer.sort()
     return answer
 
 print(solution(["ABCDE", "AB", "CD", "ADE", "XYZ", "XYZ", "ACD"], [2,3,5]))
+print(solution(["ABCD", "ABCD", "ABCD"], [2,3,4]))
 # print(solution(['AB', 'CD','CD','CD','CD','CDE' ], [1, 2, 3]))
--- PROGRAMMERS/pra.py
+++ PROGRAMMERS/pra.py
@@ -1,3 +1,5 @@
-a = 3
-if 1 <= a <= 5:
-    print(a)
+a = set()
+
+b = set([1, 2])
+
+a.add(b)
