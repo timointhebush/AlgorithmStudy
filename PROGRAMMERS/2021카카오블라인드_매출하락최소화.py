a1421383a0350d6cc35977d0f22ab779a73450cc
timointhebush <junzzang97@naver.com>
2021-09-03 22:45:33 +0900

    2021카카오블라인드 매출하락최소화 실패

--- PROGRAMMERS/2021카카오블라인드_매출하락최소화.py
+++ PROGRAMMERS/2021카카오블라인드_매출하락최소화.py
@@ -1,26 +1,47 @@
-class Staff:
-    def __init__(self, id, sales):
-        self.id = id
-        self.sales = sales
-        self.team = {"leader": self}
-
-
-class Tree:
-    def __init__(self, ceo_sales):
-        self.ceo = Staff(1, ceo_sales)
-
-
 def solution(sales, links):
-    answer = 0
-    tree = Tree(sales[0])
+    global tree, d, sales_
+    sales_ = sales
+    tree = make_tree(links)
+    d = [[-1, -1] for _ in range(len(sales) + 1)]
+    return min(get_min_sales(1, 1), get_min_sales(1, 0))
 
-    team = {}
-    links = sorted(links, key=lambda x: (x[0], x[1]))
+
+def make_tree(links):
+    tree = {}
+    for i in range(len(sales_)):
+        tree[i + 1] = []
     for link in links:
-        leader_id, staff_id = link
-        if leader_id not in team:
-            team[leader_id] = {leader_id: sales[leader_id - 1], staff_id: sales[staff_id - 1]}
-        else:
-            team[leader_id][staff_id] = sales[staff_id - 1]
+        tree[link[0]].append(link[1])
+    return tree
 
-    return answer
+
+def get_min_sales(i, root):
+    if d[i][root] != -1:
+        return d[i][root]
+    child_sum = 0
+    if root == 1:
+        for child in tree[i]:
+            child_sum += min(get_min_sales(child, 1), get_min_sales(child, 0))
+        d[i][root] = child_sum + sales_[i - 1]
+        return d[i][root]
+    else:
+        flag = False
+        diff = float("inf")
+        for child in tree[i]:
+            if get_min_sales(child, 0) > get_min_sales(child, 1):
+                flag = True
+            else:
+                diff = min(diff, get_min_sales(child, 1) - get_min_sales(child, 0))
+            child_sum += min(get_min_sales(child, 1), get_min_sales(child, 0))
+        d[i][root] = child_sum
+        if flag == False:
+            d[i][root] += diff
+        return d[i][root]
+
+
+print(
+    solution(
+        [14, 17, 15, 18, 19, 14, 13, 16, 28, 17],
+        [[10, 8], [1, 9], [9, 7], [5, 4], [1, 5], [5, 10], [10, 6], [1, 3], [10, 2]],
+    )
+)
