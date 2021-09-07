8a24cb154f4ace43d42e96b2e8b7107a1c6574df
timointhebush <junzzang97@naver.com>
2021-09-07 20:49:37 +0900

    2021카카오블라인드 카드짝맞추기 실패

--- PROGRAMMERS/2021카카오블라인드_카드짝맞추기.py
+++ PROGRAMMERS/2021카카오블라인드_카드짝맞추기.py
@@ -1,27 +1,50 @@
 from collections import deque
+from itertools import permutations, product
 
 
 def solution(board, r, c):
-    n = len(board)
-    cards = set()
+    global min_cost, cards_coord, new_board
     new_board = [[7 for _ in range(n + 2)] for _ in range(n + 2)]
     for i in range(n):
         for j in range(n):
             new_board[i + 1][j + 1] = board[i][j]
-            if board[i][j] != 0:
-                cards.add((i, j))
-    # print(get_next_moves(r + 1, c + 1, new_board))
-    queue = deque([(r + 1, c + 1, 0, [])])
-    visited = [(r + 1, c + 1, 0, [])]
-    completed_cards = set()
-    while queue:
-        r, c, num, turned = queue.popleft()
-        if set(turned) == cards:
-            return num
-        next_moves = get_next_moves(r, c, new_board)
-        for nr, nc in next_moves:
-            if (r, c) == (nr, nc):
-                n_turned = turned
+    cards_coord = {"start": (r, c)}
+    for row in range(len(board)):
+        for col in range(len(board)):
+            if board[row][col] != 0:
+                card = board[row][col]
+                if card not in cards_coord:
+                    cards_coord[card] = [(row, col)]
+                else:
+                    cards_coord[card].append((row, col))
+    # min_cost[i][j] = i에서 j로 이동하고, j와 같은 카드까지 제거하는 데 드는 최소비용
+    min_cost = [[-1 for _ in range(len(board) * 2 + 1)] for _ in range(len(board) * 2 + 1)]
+    card_cases = permutations(range(1, len(cards_coord) + 1))
+    costs = []
+    for case in card_cases:
+        order_cases = product(
+            (0,),
+            (case[0] * 2 - 1, case[0] * 2),
+            (case[1] * 2 - 1, case[1] * 2),
+            (case[2] * 2 - 1, case[2] * 2),
+        )
+        for order_case in order_cases:
+            cost = 0
+            for i in range(len(order_case) - 1):
+                cost += get_min_cost(order_case[i], order_case[i + 1])
+            costs.append(cost)
+    return min(costs)
+
+
+def get_min_cost(a, b):
+    if min_cost[a][b] != -1:
+        return min_cost[a][b]
+    if a == 0:
+        sr, sc = cards_coord["start"]
+        er, ec = cards_coord[(b + (b % 2)) / 2][b % 2]
+
+def cal_min_cost(sr, sc, er, ec, board):
+    
 
 
 def get_next_moves(r, c, new_board):
