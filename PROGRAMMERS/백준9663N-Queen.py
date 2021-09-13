2fed75d82eb0be574cece1406c4c8773ff0faee7
timointhebush <junzzang97@naver.com>
2021-09-14 00:00:38 +0900

    백준9663 성공

--- PROGRAMMERS/백준9663N-Queen.py
+++ PROGRAMMERS/백준9663N-Queen.py
@@ -1,25 +1,25 @@
 def solution():
-    global board, N
+    global board, N, cnt
     N = int(input())
     board = [-1 for _ in range(15)]
     cnt = 0
-    n_queen(0, 0)
+    n_queen(0)
     return cnt
 
 
-def n_queen(row, cnt):
+def n_queen(row):
+    global cnt
     if row == N:
         cnt += 1
-        print(cnt)
-    else:
-        for col in range(N):
-            board[row] = col
-            if promising(row):
-                n_queen(row + 1, cnt)
+        return 0
+    for col in range(N):
+        board[row] = col
+        if promising(row):
+            n_queen(row + 1)
 
 
 def promising(row):
-    for comp_r in range(row - 1):
+    for comp_r in range(row):
         if board[row] == board[comp_r] or (row - comp_r) == abs(board[row] - board[comp_r]):
             return False
     return True
