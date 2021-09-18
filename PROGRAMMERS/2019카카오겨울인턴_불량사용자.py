a3dbdf37de8953c00ebc05c7180d350be0d53307
timointhebush <junzzang97@naver.com>
2021-09-18 15:41:10 +0900

    2019카카오겨울인턴 불량사용자 성공

--- PROGRAMMERS/2019카카오겨울인턴_불량사용자.py
+++ PROGRAMMERS/2019카카오겨울인턴_불량사용자.py
@@ -1,38 +1,25 @@
+from itertools import permutations
+
+
 def solution(user_id, banned_id):
-    used = {_: False for _ in user_id}
-    cand = {_: [] for _ in banned_id}
-    no_overlap_banned = list(set(banned_id))
-    for banned in no_overlap_banned:
-        for user in user_id:
-            if check(user, banned):
-                cand[banned].append(user)
-    global answer_set, board, answer
     answer = 0
-    board = []
-    for banned in banned_id:
-        board.append(cand[banned])
-    print(board)
-    answer_set = set()
-    dfs(0)
-    return answer
+    cand = {}
+    for case in permutations(user_id, len(banned_id)):
+        if check(case, banned_id):
+            case = tuple(sorted(case))
+            if case not in cand:
+                cand[case] = True
+    return len(cand)
 
 
-def check(user, banned):
-    n_user, n_banned = len(user), len(banned)
-    if n_user != n_banned:
-        return False
-    for i in range(n_banned):
-        if banned[i] != "*" and user[i] != banned[i]:
+def check(case, banned_id):
+    for i in range(len(case)):
+        if len(case[i]) != len(banned_id[i]):
             return False
+        for j in range(len(case[i])):
+            if banned_id[i][j] != "*" and case[i][j] != banned_id[i][j]:
+                return False
     return True
-
-
-def dfs(row):
-    global answer_set
-    if row == len(board):
-        if len(answer_set) == len(board):
-            
-
 
 
 print(solution(["frodo", "fradi", "crodo", "abc123", "frodoc"], ["fr*d*", "abc1**"]))
