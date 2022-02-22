7187417cb9834db8134bae465dae85a9cfb32476
timointhebush <junzzang97@naver.com>
2022-02-23 08:04:16 +0900

    프로그래머스 여행경로 코드 리팩토링

--- PROGRAMMERS/#2/여행경로.py
+++ PROGRAMMERS/#2/여행경로.py
@@ -2,37 +2,37 @@ from collections import defaultdict
 
 
 def solution(tickets):
+    # graph 생성
     graph = defaultdict(list)
-    visited = defaultdict(list)
     for depart, arrive in tickets:
         graph[depart].append(arrive)
 
-    # 정렬
+    # graph 정렬, visited 생성
+    visited = {}
     for depart in graph.keys():
         graph[depart].sort()
         visited[depart] = [False for _ in range(len(graph[depart]))]
+
+    # check
     print("graph: ", graph)
     print("visited: ", visited)
 
+    # dfs함수 정의
     def dfs(depart, route):
         if len(route) == len(tickets) + 1:
             return route
-
         for i, arrive in enumerate(graph[depart]):
             if visited[depart][i] == False:
                 visited[depart][i] = True
                 tmp_route = route.copy()
                 tmp_route.append(arrive)
-
-                ret = dfs(arrive, tmp_route)
+                answer = dfs(arrive, tmp_route)
 
                 visited[depart][i] = False
+                if answer:
+                    return answer
 
-                if ret:
-                    return ret
-
-    answer = dfs("ICN", ["ICN"])
-    return answer
+    return dfs("ICN", ["ICN"])
 
 
 print(solution([["ICN", "JFK"], ["HND", "IAD"], ["JFK", "HND"]]))
