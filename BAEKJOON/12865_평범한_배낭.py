3b907644ee14d0bdfb0dea56f01f1b354942e363
timointhebush <junzzang97@naver.com>
2022-03-05 21:53:34 +0900

    백준 평범한 배낭

--- BAEKJOON/12865_평범한_배낭.py
+++ BAEKJOON/12865_평범한_배낭.py
@@ -1,30 +1,20 @@
 import sys
 
+
 def solution():
     N, K = tuple(map(int, sys.stdin.readline().split(" ")))
     items = []
-    max_v = 0
-    sum_w = 0
     for _ in range(N):
-        W, V = tuple(map(int, sys.stdin.readline().split(" ")))
-        if V > max_v:
-            max_v = V
-        sum_k += W
-        items.append((W, V))
-    items = sorted(items)
-    print(items)
-    left, right = 0, max_v
-    answer = 0
-    while left <= right:
-        mid = (left + right) // 2
-        bag_w = 0
-        bag_v = 0
-        for W, V in items:
-            if V < mid:
+        items.append(tuple(map(int, sys.stdin.readline().split(" "))))
+    dp = [[0 for _ in range(K + 1)] for _ in range(N + 1)]
+    for n in range(1, N + 1):
+        w, v = items[n - 1]
+        for k in range(1, K + 1):
+            if w > k:
+                dp[n][k] = dp[n - 1][k]
+            else:
+                dp[n][k] = max(dp[n - 1][k - w] + v, dp[n - 1][k])
+    return dp[N][K]
 
-            bag_w += W
-            bag_v += V
 
-    
-
-solution()
+print(solution())
