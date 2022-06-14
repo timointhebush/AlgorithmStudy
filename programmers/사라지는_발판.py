3ab0c393f22aa4eed66ee3ba7233e524629b87d4
이준석 <junzzang97@naver.com>
2022-06-14 13:34:40 +0900

    프로그래머스 사라지는 발판 2차

--- programmers/사라지는_발판.py
+++ programmers/사라지는_발판.py
@@ -1,48 +1,46 @@
-dr = [0, 0, -1, 1]
-dc = [-1, 1, 0, 0]
-R, C = 0, 0
+dr = [-1, 1, 0, 0]
+dc = [0, 0, -1, 1]
 
-block = [[0 for _ in range(5)] for _ in range(5)]
-visited = [[0 for _ in range(5)] for _ in range(5)]
-
-
-def is_out(r, c):
-    return r < 0 or r >= R or c < 0 or c >= C
-
-
-def get_left_move(cur_r, cur_c, op_r, op_c):
-    global visited, block
-    if visited[cur_r][cur_c]:
-        return 0
-    result = 0
-    for dir in range(4):
-        nxt_r = cur_r + dr[dir]
-        nxt_c = cur_c + dc[dir]
-        if is_out(nxt_r, nxt_c) or visited[nxt_r][nxt_c] or block[nxt_r][nxt_c] == 0:
-            continue
-        visited[cur_r][cur_c] = 1
-        value = get_left_move(op_r, op_c, nxt_r, nxt_c) + 1
-        visited[cur_r][cur_c] = 0
-
-        if result % 2 == 0 and value % 2 == 1:
-            result = value
-        elif result % 2 == 0 and value % 2 == 0:
-            result = max(result, value)
-        elif result % 2 == 1 and value % 2 == 1:
-            result = min(result, value)
-
-    return result
+game_board = []
+visited = []
 
 
 def solution(board, aloc, bloc):
-    global R, C
-    R, C = len(board), len(board[0])
-    for r in range(R):
-        for c in range(C):
-            block[r][c] = board[r][c]
-    a_r, a_c = aloc
-    b_r, b_c = bloc
-    return get_left_move(a_r, a_c, b_r, b_c)
+    global game_board, visited
+    game_board = board
+    visited = [[0] * len(board[0]) for _ in range(len(board))]
+    print(visited)
+    return get_sum_of_move(aloc[0], aloc[1], bloc[0], bloc[1])
 
 
-print(solution(	[[1, 1, 1], [1, 1, 1], [1, 1, 1]], [1, 0], [1, 2]))
+def get_sum_of_move(cur_r, cur_c, opp_r, opp_c):
+    global game_board, visited
+    cur_sum_of_move = 0
+    if visited[cur_r][cur_c]:
+        return cur_sum_of_move
+    for dir in range(4):
+        nxt_cur_r, nxt_cur_c = cur_r + dr[dir], cur_c + dc[dir]
+        if not is_movable(nxt_cur_r, nxt_cur_c):
+            continue
+        visited[cur_r][cur_c] = 1
+        future_sum_of_move = get_sum_of_move(opp_r, opp_c, nxt_cur_r, nxt_cur_c) + 1
+        visited[cur_r][cur_c] = 0
+        if not is_winner(cur_sum_of_move) and is_winner(future_sum_of_move):
+            cur_sum_of_move = future_sum_of_move
+        elif not is_winner(cur_sum_of_move) and not is_winner(future_sum_of_move):
+            cur_sum_of_move = max(cur_sum_of_move, future_sum_of_move)
+        elif is_winner(cur_sum_of_move) and is_winner(future_sum_of_move):
+            cur_sum_of_move = min(cur_sum_of_move, future_sum_of_move)
+    return cur_sum_of_move
+
+
+def is_movable(r, c):
+    return 0 <= r < len(game_board) and 0 <= c < len(game_board[0])\
+           and game_board[r][c] and not visited[r][c]
+
+
+def is_winner(move):
+    return move % 2 == 1
+
+
+print(solution([[1, 1, 1], [1, 1, 1], [1, 1, 1]], [1, 0], [1, 2]))
