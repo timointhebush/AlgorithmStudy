ebe136fb7e17f9bcb9347df1e6160b1eb4e3dcb7
timointhebush <junzzang97@naver.com>
2022-06-20 23:26:46 +0900

    프로그래머스 징검다리 건너기 연습

--- programmers/징검다리_건너기.py
+++ programmers/징검다리_건너기.py
@@ -1,23 +1,18 @@
 def solution(stones, k):
-    left, right = 1, 200000
+    left, right = 1, 200000000
     while left < right:
         mid = (left + right) // 2
-        max_empty = 0
-        j = -1
-        for i in range(len(stones)):
-            if i > j and (stones[i] - mid) <= 0:
-                n_empty = 1
-                j = i + 1
-                while j < len(stones) and (stones[j] - mid) <= 0:
-                    n_empty += 1
-                    j += 1
-                max_empty = max(max_empty, n_empty)
-
-        if max_empty <= k:
-            left = mid
-        else: # max_empty > k:
+        empty_cnt = 0
+        for stone in stones:
+            if (stone - mid) <= 0:
+                empty_cnt += 1
+            else:
+                empty_cnt = 0
+        if k <= empty_cnt: # 줄임.
             right = mid - 1
-    return left
+        else:
+            left = mid + 1
+    return left - 1
 
 
 print(solution([2, 4, 5, 3, 2, 1, 4, 2, 5, 1], 3))
