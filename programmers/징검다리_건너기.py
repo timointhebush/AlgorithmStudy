0c23ee3971ed2086f4d597e70ec507977e965175
이준석 <junzzang97@naver.com>
2022-07-04 21:18:56 +0900

    프로그래머스 징검다리 건너기

--- programmers/징검다리_건너기.py
+++ programmers/징검다리_건너기.py
@@ -1,18 +1,26 @@
 def solution(stones, k):
-    left, right = 1, 200000000
-    while left < right:
+    answer = 1
+    left, right = 1, 2 * (10 ** 8)
+    while left <= right:
         mid = (left + right) // 2
-        empty_cnt = 0
-        for stone in stones:
-            if (stone - mid) <= 0:
-                empty_cnt += 1
-            else:
-                empty_cnt = 0
-        if k <= empty_cnt: # 줄임.
-            right = mid - 1
-        else:
+        if is_crossable(stones, k, mid):
             left = mid + 1
-    return left - 1
+            answer = max(answer, mid)
+        else:
+            right = mid - 1
+    return answer
+
+
+def is_crossable(stones, k, mid):
+    broken_stone = 0
+    for stone in stones:
+        if stone - mid < 0:
+            broken_stone += 1
+        else:
+            broken_stone = 0
+        if broken_stone == k:
+            return False
+    return True
 
 
 print(solution([2, 4, 5, 3, 2, 1, 4, 2, 5, 1], 3))
