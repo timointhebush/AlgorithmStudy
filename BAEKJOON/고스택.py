d07681aff595b3be6c3a34f3dca60538b877bd1f
이준석 <jslee02@miridih.com>
2022-07-23 23:32:27 +0900

    백준 고스택

--- BAEKJOON/고스택.py
+++ BAEKJOON/고스택.py
@@ -1,21 +1,19 @@
-def solve():
-    for command in command_list:
-        if len(command) > 3:
-            command, x = command.split(" ")
-            x = int(x)
-        func = command_to_func[command]
-        if command == "NUM":
-            func(x)
-        else:
-            func()
-        if is_error:
-            print("ERROR")
-            return
+import sys
 
-    if is_error or len(stack) != 1:
-        print("ERROR")
-    else:
+def solve():
+    try:
+        for command in command_list:
+            command = command.split()
+            func = command_to_func[command[0]]
+            if len(command) > 1:
+                func(int(command[1]))
+            else:
+                func()
+        if len(stack) != 1:
+            raise IndexError
         print(stack[-1])
+    except (ValueError, IndexError, ZeroDivisionError):
+        print("ERROR")
 
 
 def num(x):
@@ -23,34 +21,21 @@ def num(x):
 
 
 def pop():
-    global is_error
-    if len(stack) < 1:
-        is_error = True
-        return
     stack.pop()
 
 
 def inv():
-    global is_error
-    if len(stack) < 1:
-        is_error = True
-        return
-    stack[-1] = -stack[-1]
+    val = stack.pop()
+    stack.append(-1 * val)
 
 
 def dup():
-    global is_error
-    if len(stack) < 1:
-        is_error = True
-        return
-    stack.append(stack[-1])
+    val = stack.pop()
+    stack.append(val)
+    stack.append(val)
 
 
 def swp():
-    global is_error
-    if len(stack) < 2:
-        is_error = True
-        return
     first = stack.pop()
     second = stack.pop()
     stack.append(first)
@@ -58,80 +43,78 @@ def swp():
 
 
 def add():
-    global is_error
-    if len(stack) < 2:
-        is_error = True
-        return
     first = stack.pop()
     second = stack.pop()
     val = first + second
-    if abs(val) >= 10 ** 9:
-        is_error = True
-        return
+    check_val(val)
     stack.append(val)
 
 
 def sub():
-    global is_error
-    if len(stack) < 2:
-        is_error = True
-        return
     first = stack.pop()
     second = stack.pop()
     val = second - first
-    if abs(val) >= 10 ** 9:
-        is_error = True
-        return
+    check_val(val)
     stack.append(val)
 
 
 def mul():
-    global is_error
-    if len(stack) < 2:
-        is_error = True
-        return
     first = stack.pop()
     second = stack.pop()
     val = second * first
-    if abs(val) >= 10 ** 9:
-        is_error = True
-        return
+    check_val(val)
     stack.append(val)
 
 
 def div():
-    global is_error
-    if len(stack) < 2:
-        is_error = True
-        return
     first = stack.pop()
     second = stack.pop()
-    if first == 0:
-        is_error = True
-        return
     val = abs(second) // abs(first)
     if first * second < 0:
         val *= -1
+    check_val(val)
     stack.append(val)
 
 
 def mod():
-    global is_error
-    if len(stack) < 2:
-        is_error = True
-        return
     first = stack.pop()
     second = stack.pop()
-    if first == 0:
-        is_error = True
-        return
     val = abs(second) % abs(first)
     if second < 0:
         val *= -1
+    check_val(val)
     stack.append(val)
 
 
+def until_quit():
+    global command_list, stack
+    while True:
+        command_list = []
+        while True:
+            command = input()
+            if command == "QUIT":
+                return 0
+            elif command != "END":
+                command_list.append(command)
+            else:
+                break
+
+        N = int(input())
+        for _ in range(N):
+            stack = [int(input())]
+            solve()
+        empty = input()
+        print(empty)
+
+
+def check_val(val):
+    if abs(val) > 10 ** 9:
+        raise ValueError
+
+
 if __name__ == "__main__":
+    sys.stdin = open("고스택.txt", "r")
+
     command_to_func = {
         "NUM": num,
         "POP": pop,
@@ -144,26 +127,7 @@ if __name__ == "__main__":
         "DIV": div,
         "MOD": mod
     }
-    is_quit = False
-    while True:
-        is_error = False
-        command_list = []
-        while True:
-            command = input()
-            if command == "END":
-                break
-            if command == "QUIT":
-                is_quit = True
-                break
-            command_list.append(command)
 
-        if is_quit:
-            break
-
-        stack = []
-        N = int(input())
-        for i in range(N):
-            stack = [int(input())]
-            solve()
-        input()
-        print()
+    command_list = []
+    stack = []
+    until_quit()
