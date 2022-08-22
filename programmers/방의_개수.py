a2f8bc73e767b0bb9df6ad09bf571f6f0696f36e
이준석 <jslee02@miridih.com>
2022-08-22 23:00:11 +0900

    프로그래머스 방의 개수

--- programmers/방의_개수.py
+++ programmers/방의_개수.py
@@ -1,40 +1,35 @@
+from collections import defaultdict, deque
+
+dr = [-1, -1, 0, 1, 1, 1, 0, -1]
+dc = [0, 1, 1, 1, 0, -1, -1, -1]
+
+
 def solution(arrows):
     answer = 0
-    routes = set()
-    points = set()
-    points.add((0, 0))
-    x, y = 0, 0
+    r, c = 0, 0
+    q = deque()
+    is_visited_coord = defaultdict(bool)
+    is_visited_from_to = defaultdict(bool)
+
     for arrow in arrows:
-        dx, dy = get_delta(arrow)
-        mx, my = x + dx, y + dy
-        nx, ny = x + 2 * dx, y + 2 * dy
-        if (mx, my) in points and ((x, y), (mx, my)) not in routes:
-            answer += 1
-        if (nx, ny) in points and ((mx, my), (nx, ny)) not in routes:
-            answer += 1
-        routes.add(((x, y), (mx, my)))
-        routes.add(((mx, my), (x, y)))
-        routes.add(((mx, my), (nx, ny)))
-        routes.add(((nx, ny), (mx, my)))
-        points.add((mx, my))
-        points.add((nx, ny))
-        x, y = nx, ny
+        for _ in range(2):
+            r, c = r + dr[arrow], c + dc[arrow]
+            q.append((r, c))
+
+    r, c = 0, 0
+    is_visited_coord[(r, c)] = True
+    while q:
+        nr, nc = q.popleft()
+        if is_visited_coord[(nr, nc)]:
+            if not is_visited_from_to[((r, c), (nr, nc))]:
+                answer += 1
+        else:
+            is_visited_coord[(nr, nc)] = True
+        is_visited_from_to[((r, c), (nr, nc))] = True
+        is_visited_from_to[((nr, nc), (r, c))] = True
+        r, c = nr, nc
     return answer
 
 
-def get_delta(arrow):
-    delta = {
-        0: (0, 1),
-        1: (1, 1),
-        2: (1, 0),
-        3: (1, -1),
-        4: (0, -1),
-        5: (-1, -1),
-        6: (-1, 0),
-        7: (-1, 1),
-    }
-    return delta[arrow]
-
-
-print(solution([2, 4, 6, 4, 0, 2, 0, 6]))
-print(solution([2, 4, 6, 0, 3, 6, 0, 2, 5]))
+if __name__ == "__main__":
+    print(solution([6, 6, 6, 4, 4, 4, 2, 2, 2, 0, 0, 0, 1, 6, 5, 5, 3, 6, 0]))
