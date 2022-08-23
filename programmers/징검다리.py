d5b1a3b9395282d2039d2cd8e5ade13e75047170
이준석 <jslee02@miridih.com>
2022-08-23 22:01:38 +0900

    프로그래머스 징검다리

--- programmers/징검다리.py
+++ programmers/징검다리.py
@@ -1,24 +1,27 @@
 def solution(distance, rocks, n):
-    rocks.sort()
-    rocks.append(distance)
-    left, right = 0, distance
     answer = 0
+    left, right = 1, distance
+    rocks.sort()
     while left <= right:
-        removed_rocks = 0
-        mid = (left + right) // 2
-        left_rock = 0
-        for right_rock in rocks:
-            if (right_rock - left_rock) < mid:
-                removed_rocks += 1
+        tmp_min_gap = (left + right) // 2
+        pre_rock = 0
+        tmp_n = 0
+        is_over = False
+        for rock in rocks:
+            if rock - pre_rock < tmp_min_gap:
+                tmp_n += 1
+                if tmp_n > n:
+                    is_over = True
+                    break
             else:
-                left_rock = right_rock
-        if removed_rocks > n:
-            right = mid - 1
+                pre_rock = rock
+        if is_over:
+            right = tmp_min_gap - 1
         else:
-            if mid > answer:
-                answer = mid
-            left = mid + 1
+            answer = max(answer, tmp_min_gap)
+            left = tmp_min_gap + 1
     return answer
 
 
-print(solution(25, [2, 14, 11, 21, 17], 2))
+if __name__ == "__main__":
+    print(solution(25, [2, 14, 11, 21, 17], 2))
