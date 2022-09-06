e9296cc4e783873b943effd9b240c3b69f9c7672
이준석 <jslee02@miridih.com>
2022-09-06 23:02:56 +0900

    프로그래머스 카드 짝 맞추기

--- programmers/카드_짝_맞추기.py
+++ programmers/카드_짝_맞추기.py
@@ -1,102 +1,96 @@
-from collections import defaultdict, deque
-from itertools import permutations, product
 import copy
+from itertools import permutations, combinations, product
+from collections import defaultdict, deque
+
+dr = [0, 0, -1, 1]
+dc = [1, -1, 0, 0]
+
+cache = {}
 
 
 def solution(board, r, c):
     answer = float('inf')
-    card_to_coords_1 = defaultdict(list)
-    card_to_coords_2 = defaultdict(list)
-    cards = set()
-    for i in range(len(board)):
-        for j in range(len(board[0])):
-            if board[i][j] != 0:
-                card = board[i][j]
-                cards.add(card)
-                card_to_coords_1[card].append((i, j))
-    for card, coords in card_to_coords_1.items():
-        coords_2 = [coords[1], coords[0]]
-        card_to_coords_2[card] = coords_2
+    card_to_coords = defaultdict(list)
+    for nr in range(4):
+        for nc in range(4):
+            if board[nr][nc] != 0:
+                card_to_coords[board[nr][nc]].append((nr, nc))
 
-    cards = list(cards)
-    card_cases = permutations(cards, len(cards))
-    cache = {}
+    for key in card_to_coords.keys():
+        tmp = card_to_coords[key]
+        tmp_2 = [tmp[1], tmp[0]]
+        card_to_coords[key] = [tmp, tmp_2]
+
+    card_num = len(card_to_coords.keys())
+    type_cases = list(product(range(2), repeat=card_num))
+    card_cases = list(permutations(list(card_to_coords.keys()), card_num))
+
     for card_case in card_cases:
-        final_cases = create_final_case(card_case, card_to_coords_1, card_to_coords_2)
-        for final_case in final_cases:
-            # print("case: ", final_case)
-            cur = (r, c)
-            case_moves = 0
-            c_board = copy.deepcopy(board)
-            for i, nxt in enumerate(final_case):
-                move = get_min_move(cur, nxt, c_board, cache)
-                # print(cur, " to ", nxt)
-                # print("move: ", move)
-                case_moves += move
-                if (i + 1) % 2 == 0:
-                    c_board[cur[0]][cur[1]] = 0
-                    c_board[nxt[0]][nxt[1]] = 0
-                cur = nxt
-            # if case_moves == 9:
-            #     print("----------------------")
-            if case_moves < answer:
-                answer = case_moves
-    return answer + (2 * len(cards))
+        for type_case in type_cases:
+            card_seq = [(r, c)]
+            for i in range(card_num):
+                card, type = card_case[i], type_case[i]
+                card_seq.extend(card_to_coords[card][type])
+
+            deleted = set()
+            tmp_answer = 0
+            copy_board = copy.deepcopy(board)
+            tmp_answer += (get_min(copy_board, card_seq[0], card_seq[1], deleted) + 1)
+            for i in range(1, len(card_seq) - 1):
+                a, b = card_seq[i], card_seq[i + 1]
+                move = get_min(copy_board, a, b, deleted)
+                tmp_answer += move
+                tmp_answer += 1
+                if i % 2 != 0:
+                    copy_board[a[0]][a[1]] = 0
+                    copy_board[b[0]][b[1]] = 0
+                    deleted.add(a)
+                    deleted.add(b)
+                if tmp_answer > answer:
+                    break
+            answer = min(answer, tmp_answer)
+    return answer
 
 
-def create_final_case(card_case, coords_1, coords_2):
-    num_to_coords = {0: coords_1, 1: coords_2}
-    num_cases = product(range(2), repeat=len(card_case))
-    final_cases = []
-    for case in num_cases:
-        final_case = []
-        for i in range(len(case)):
-            card = card_case[i]
-            coords = num_to_coords[case[i]]
-            final_case.extend(coords[card])
-        final_cases.append(final_case)
-    return final_cases
-
-
-def get_min_move(cur, nxt, board, cache):
-    if cur == nxt:
+def get_min(board, a, b, deleted):
+    if a == b:
         return 0
 
-    depth = 0
-    q = deque([(cur[0], cur[1], depth)])
-    visited = {cur}
+    tmp_key = sorted(list(deleted))
+    tmp_key.extend([a, b])
+    key = tuple(tmp_key)
+    if key in cache:
+        return cache[key]
+
+    q = deque([(a[0], a[1], 0)])
+    visited = set()
     while q:
-        r, c, depth = q.popleft()
-        cases = _get_all_move_cases((r, c), board)
-        for cr, cc in cases:
-            if (cr, cc) == nxt:
-                return depth + 1
-            if (0 <= cr <= 3 and 0 <= cc <= 3) and (cr, cc) not in visited:
-                visited.add((cr, cc))
-                q.append((cr, cc, depth + 1))
+        r, c, num = q.popleft()
+        for i in range(4):
+            nr, nc = r + dr[i], c + dc[i]
+            if 0 <= nr < 4 and 0 <= nc < 4 and (nr, nc):
+                if (nr, nc) == b:
+                    cache[key] = num + 1
+                    return num + 1
+                if (nr, nc) not in visited:
+                    visited.add((nr, nc))
+                    q.append((nr, nc, num + 1))
+        for i in range(4):
+            nr, nc = r + dr[i], c + dc[i]
+            is_card = False
+            while 0 <= nr < 4 and 0 <= nc < 4:
+                if (nr, nc) == b:
+                    cache[key] = num + 1
+                    return num + 1
+                if board[nr][nc] != 0:
+                    q.append((nr, nc, num + 1))
+                    is_card = True
+                    break
+                nr, nc = nr + dr[i], nc + dc[i]
+            if not is_card:
+                q.append((nr - dr[i], nc - dc[i], num + 1))
 
 
-def _get_all_move_cases(coords, board):
-    r, c = coords
-    delta = [(1, 0), (-1, 0), (0, 1), (0, -1)]
-    one_move = [(r + 1, c), (r - 1, c), (r, c + 1), (r, c - 1)]
-    ctrl_move = []
-    for dr, dc in delta:
-        k = 1
-        while True:
-            nr, nc = r + dr*k, c + dc*k
-            if nr < 0 or nr > 3 or nc < 0 or nc > 3:
-                ctrl_move.append((nr - dr, nc - dc))
-                break
-            if board[nr][nc] != 0:
-                ctrl_move.append((nr, nc))
-                break
-            k += 1
-
-    cases = one_move + ctrl_move
-    return list(set(cases))
-
-import time
-start = time.time()
-print(solution([[1,0,0,3],[2,0,0,0],[0,0,0,2],[3,0,1,0]], 1, 0))
-print(time.time() - start)
+if __name__ == "__main__":
+    print(solution([[1,0,0,3],[2,0,0,0],[0,0,0,2],[3,0,1,0]], 1, 0))
+    print(solution([[3,0,0,2],[0,0,1,0],[0,1,0,0],[2,0,0,3]], 0, 1))
