62fff9b3e2a4ff4f3f0138029ce63b4fa85cb861
이준석 <jslee02@miridih.com>
2022-09-09 16:16:59 +0900

    프로그래머스 양과 늑대

--- programmers/양과_늑대.py
+++ programmers/양과_늑대.py
@@ -1,38 +1,42 @@
 from collections import defaultdict
 import copy
 
-graph = None
-is_wolf = None
+graph = defaultdict(list)
+is_wolf = []
 answer = 0
 
 
 def solution(info, edges):
-    global graph, is_wolf, answer
-    graph = defaultdict(list)
+    global graph, is_wolf
     is_wolf = info
-    for parent, child in edges:
-        graph[parent].append(child)
-        graph[child].append(parent)
-    dfs(0, set(), {0: 0, 1: 0}, [])
+    visited = [False for _ in range(len(info))]
+    for e in edges:
+        graph[e[0]].append(e[1])
+        graph[e[1]].append(e[0])
+    dfs(0, 0, 0, [], visited)
     return answer
 
 
-def dfs(current_node, visited, animal, visitable_nodes):
-    global graph, is_wolf, answer
-    visited.add(current_node)
-    animal[is_wolf[current_node]] += 1
-    if animal[0] <= animal[1]:
+def dfs(node, sheep_num, wolf_num, prev_nodes, visited):
+    global answer
+    if not visited[node]:
+        if is_wolf[node]:
+            wolf_num += 1
+        else:
+            sheep_num += 1
+            answer = max(answer, sheep_num)
+        visited[node] = True
+
+    if wolf_num >= sheep_num:
         return
-    if answer < animal[0]:
-        answer = animal[0]
-    nxt_visitable_nodes = []
-    connected_nodes = graph[current_node]
-    for node in (visitable_nodes + connected_nodes):
-        if node not in visited and node != current_node:
-            nxt_visitable_nodes.append(node)
-    for nxt_node in nxt_visitable_nodes:
-        dfs(nxt_node, copy.deepcopy(visited), copy.deepcopy(animal), nxt_visitable_nodes)
+
+    prev_nodes.extend(graph[node])
+    for nxt_node in prev_nodes:
+        if not visited[nxt_node]:
+            nxt_prev_nodes = [prev_node for prev_node in prev_nodes if prev_node != nxt_node and not visited[prev_node]]
+            dfs(nxt_node, sheep_num, wolf_num, nxt_prev_nodes, copy.deepcopy(visited))
 
 
-# print(solution(	[0, 0, 1, 1, 1, 0, 1, 0, 1, 0, 1, 1], [[0, 1], [1, 2], [1, 4], [0, 8], [8, 7], [9, 10], [9, 11], [4, 3], [6, 5], [4, 6], [8, 9]]))
-# print(solution([0, 0], [[0, 1]]))
+if __name__ == "__main__":
+    print(solution([0, 0, 1, 1, 1, 0, 1, 0, 1, 0, 1, 1],
+                   [[0, 1], [1, 2], [1, 4], [0, 8], [8, 7], [9, 10], [9, 11], [4, 3], [6, 5], [4, 6], [8, 9]]))
