248c0270b3a0f0f05943cc27e68fb30eb2030de5
이준석 <jslee02@miridih.com>
2022-09-10 17:13:47 +0900

    프로그래머스 선입 선출 스케줄링

--- programmers/선입_선출_스케줄링.py
+++ programmers/선입_선출_스케줄링.py
@@ -1,25 +1,32 @@
 def solution(n, cores):
-    if n <= len(cores):
+    num_cores = len(cores)
+    if n <= num_cores:
         return n
-    n -= len(cores)
-    left, right = 1, max(cores) * n
-    while left < right:
-        mid = (left + right) // 2
-        num_job_done = 0
-        for core in cores:
-            num_job_done += mid // core
-        if n <= num_job_done: # 일을 더많이 -> 시간을 줄여야 함.
-            right = mid
-        else: # n >= num_job_done : 일을 적게함 -> 시간을 늘려야함
-            left = mid + 1
-    time_job_done = right
-    for core in cores:
-        n -= (time_job_done - 1) // core
-    for i, core in enumerate(cores):
-        if time_job_done % core == 0:
-            n -= 1
-            if n == 0:
+
+    n -= num_cores
+
+    min_time, max_time = 1, max(cores) * n
+    while min_time < max_time:
+        tmp_time = (min_time + max_time) // 2
+        num_task_done = 0
+        for core_time in cores:
+            num_task_done += tmp_time // core_time
+
+        if num_task_done < n:
+            min_time = tmp_time + 1
+        else:
+            max_time = tmp_time
+
+    left_task = n
+    for core_time in cores:
+        left_task -= (max_time - 1) // core_time
+
+    for i, core_time in enumerate(cores):
+        if max_time % core_time == 0:
+            left_task -= 1
+            if left_task == 0:
                 return i + 1
 
 
-print(solution(6, [1, 2, 3]))
+if __name__ == "__main__":
+    print(solution(6, [1, 2, 3]))
