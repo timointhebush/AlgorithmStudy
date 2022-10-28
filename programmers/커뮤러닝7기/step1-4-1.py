cd1bf6d6bbfd39cbb38dc0cab04d34890f1235ea
이준석 <jslee02@miridih.com>
2022-10-28 10:01:23 +0900

    프로그래머스 스텝141

--- programmers/커뮤러닝7기/step1-4-1.py
+++ programmers/커뮤러닝7기/step1-4-1.py
@@ -1,11 +1,14 @@
 def solution(number, k):
-    splitted_num_list = [int(n) for n in number]
-    ordered_splitted_num_list = sorted(splitted_num_list)
-
-    for ki in range(k):
-        remove_target_num = ordered_splitted_num_list[ki]
-        number = number.replace(str(remove_target_num), "", 1)
-    return number
+    answer = []
+    for n in number:
+        while len(answer) > 0 and k > 0 and answer[-1] < n:
+            answer.pop()
+            k -= 1
+        answer.append(n)
+    if k > 0:
+        return "".join(answer[:-k])
+    else:
+        return "".join(answer)
 
 
 if __name__ == "__main__":
